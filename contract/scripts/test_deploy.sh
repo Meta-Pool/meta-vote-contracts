41aef0796c19dc8753039a8071eaf4cd7b251ed5
José María Sosa Resendiz <jose.skedar@gmail.com>
2022-06-20 09:34:02 -0500

    Changing the min locking period

--- contract/scripts/test_deploy.sh
+++ contract/scripts/test_deploy.sh
@@ -1,3 +1,6 @@
 
 NEAR_ENV=testnet near deploy -f --wasmFile res/meta_vote_contract.wasm --accountId metavote.testnet
 NEAR_ENV=testnet near view metavote.testnet get_all_locking_positions '{"voter_id": "test123512.testnet"}' --accountId metavote.testnet
+
+
+NEAR_ENV=testnet near view $CONTRACT_NAME get_project_details '{"kickstarter_id": 0}' --accountId $SUPPORTER_ID
