38c7cf8012fd3554817c8fb91f4e2cfda7070a7d
saguero <sergiogaguero@gmail.com>
2022-08-02 11:49:29 -0300

    fix tab top position

--- front/pages/components/MyDashboard/index.tsx
+++ front/pages/components/MyDashboard/index.tsx
@@ -16,7 +16,7 @@ const MyDashboardPage = (props: Props) => {
         <DashboardHeader></DashboardHeader>
         <Tabs px={{base:'5px', md: '10%'}}  colorScheme='black ' mb={50}>
 
-          <TabList border={'2px solid white'} w={'fit-content'} borderRadius={100} bg={'white'} position={{base: 'inherit', md: 'relative'}} top={{base: 0 , md:-10}}>
+          <TabList border={'2px solid white'} w={'fit-content'} borderRadius={100} bg={'white'} position={{base: 'inherit', md: 'relative'}} top={{base: 0 , md:-20}}>
             <Tab  borderRadius={100} py={'20px'} px={'32px'} borderColor={'white'}  _selected={{textUnderlineOffset: '10px', color: 'white' , bg: colors.secundary + '.500', textDecor: 'none' }} fontSize={{base: '16px', md: '24px'}}>My Voting Power</Tab>
             <Tab  borderRadius={100} py={'20px'} px={'32px'} borderColor={'white'}  _selected={{textUnderlineOffset: '10px', color: 'white' , bg: colors.secundary + '.500', textDecor: 'none' }} fontSize={{base: '16px', md: '24px'}}>My Votes</Tab>
           </TabList>
