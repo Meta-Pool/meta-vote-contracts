0bf4e8c47e70dc75849515c6d06cd269cfdc0d6e
saguero <sergiogaguero@gmail.com>
2022-08-02 11:17:36 -0300

    new tab design

--- front/pages/components/MyDashboard/index.tsx
+++ front/pages/components/MyDashboard/index.tsx
@@ -3,6 +3,7 @@ import DashboardHeader from './DashHeader';
 import LockingPosition from './LockingPositions';
 import ListingVotes from './ListingVotes';
 import { Container, Stack, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { colors } from "../../../constants/colors";
 
 
 type Props = {
@@ -11,13 +12,13 @@ type Props = {
 
 const MyDashboardPage = (props: Props) => {
   return (
-      <Stack id="dashboard">
+      <Stack spacing={{base: 50, md: 50}} id="dashboard">
         <DashboardHeader></DashboardHeader>
-        <Tabs  colorScheme='black ' mt={50} mb={50}>
+        <Tabs px={{base:'5px', md: '10%'}}  colorScheme='black ' mb={50}>
 
-          <TabList  >
-            <Tab  borderTopRadius={'32px'} p={'32px'} _selected={{textUnderlineOffset: '10px', color: 'black' , bg: { base: 'none', md: 'white'}, textDecor: { base: 'underline', md: 'none'} }} fontSize={{base: 'xl', md: '4xl'}} >My Voting Power</Tab>
-            <Tab  borderTopRadius={'32px'} p={'32px'} _selected={{textUnderlineOffset: '10px', color: 'black' , bg: { base: 'none', md: 'white'}, textDecor: { base: 'underline', md: 'none'} }} fontSize={{base: 'xl', md: '4xl'}}>My Votes</Tab>
+          <TabList border={'2px solid white'} w={'fit-content'} borderRadius={100} bg={'white'} position={{base: 'inherit', md: 'relative'}} top={{base: 0 , md:-10}}>
+            <Tab  borderRadius={100} py={'20px'} px={'32px'} borderColor={'white'}  _selected={{textUnderlineOffset: '10px', color: 'white' , bg: colors.secundary + '.500', textDecor: 'none' }} fontSize={{base: '16px', md: '24px'}}>My Voting Power</Tab>
+            <Tab  borderRadius={100} py={'20px'} px={'32px'} borderColor={'white'}  _selected={{textUnderlineOffset: '10px', color: 'white' , bg: colors.secundary + '.500', textDecor: 'none' }} fontSize={{base: '16px', md: '24px'}}>My Votes</Tab>
           </TabList>
           <TabPanels backgroundColor={{md: 'white'}} >
             <TabPanel>
