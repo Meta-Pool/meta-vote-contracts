52ae9d66fd85b1e3109dc245c34e92183a1c5d67
the-preem-palver <the.preem.palver@protonmail.com>
2022-09-03 17:32:12 -0300

    make env=development the default if no env values

--- front/constants/index.tsx
+++ front/constants/index.tsx
@@ -1,5 +1,5 @@
 
- export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_METAVOTE;
+export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_METAVOTE||"metayield.app";
 
 export const enum ACTION_TYPE {
     RELOCK,
