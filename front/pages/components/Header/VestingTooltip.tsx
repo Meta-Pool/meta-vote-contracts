643eda0d55608c2335f1217190767a13a26cf707
Leandro Manzanal <leomanzanal@gmail.com>
2023-01-05 20:52:23 -0300

    added unlocked amount

--- front/pages/components/Header/VestingTooltip.tsx
+++ front/pages/components/Header/VestingTooltip.tsx
@@ -20,11 +20,19 @@ export interface VestingInfoProps {
 }
 interface Props extends TextProps {
   vestingInfo?: VestingInfoProps;
+  balance: number;
 }
-const VestingTooltip = ({ vestingInfo, children, ...props }: Props) => {
+const VestingTooltip = ({
+  vestingInfo,
+  balance,
+  children,
+  ...props
+}: Props) => {
   return vestingInfo ? (
-    <Tooltip w="full" minW="390px"
-      label={<TooltipText vestingInfo={vestingInfo} />}
+    <Tooltip
+      w="full"
+      minW="390px"
+      label={<TooltipText vestingInfo={vestingInfo} balance={balance} />}
       placement="bottom"
       defaultIsOpen
       closeDelay={100}
@@ -38,19 +46,27 @@ const VestingTooltip = ({ vestingInfo, children, ...props }: Props) => {
   );
 };
 
-const TooltipText = ({ vestingInfo }: { vestingInfo: VestingInfoProps }) => (
-  <Text>
-    You have a lockup account
-    <br />
-    Gradual relase starts{" "}
-    {formatTimestampToDate(
-      Number(vestingInfo.linear_start_timestamp) * 1000
-    )}{" "}
-    and ends{" "}
-    {formatTimestampToDate(Number(vestingInfo.linear_end_timestamp) * 1000)}
-    <br />
-    You still have {formatToLocaleNear(yton(vestingInfo.locked))} $META locked
-    in the account
-  </Text>
-);
+const TooltipText = ({ vestingInfo, balance }: Props) => {
+  const unlockedBalance =
+    Number(balance) - Number(yton(vestingInfo?.locked!)) > 0
+      ? Number(balance) - Number(yton(vestingInfo?.locked!))
+      : 0;
+  return (
+    <Text>
+      You have a lockup account
+      <br />
+      Gradual release starts{" "}
+      {formatTimestampToDate(
+        Number(vestingInfo!.linear_start_timestamp) * 1000
+      )}{" "}
+      and ends{" "}
+      {formatTimestampToDate(Number(vestingInfo!.linear_end_timestamp) * 1000)}
+      <br />
+      You have {formatToLocaleNear(yton(vestingInfo!.locked))} $META locked{" "}
+      <br />
+      You have {formatToLocaleNear(Number(unlockedBalance))} $META unlocked for
+      transfer in the account
+    </Text>
+  );
+};
 export default VestingTooltip;
