ac08320d6d8efa5d9fb6bdfd80ea4ab563bb8069
the-preem-palver <the.Preem.Palver@protonmail.com>
2024-04-14 09:15:48 -0300

    fix redeploy script

--- scripts/mainnet/deploy_meta_vote.sh
+++ scripts/mainnet/deploy_meta_vote.sh
@@ -16,12 +16,12 @@ TOTAL_PREPAID_GAS="300000000000000"
 # NEAR_ENV=testnet near deploy --wasmFile $METAVOTE_WASM  --initFunction new --initArgs '{"admin_id": "'$ADMIN_ADDRESS'", "operator_id": "'$OPERATOR_ADDRESS'", "meta_token_contract_address": "'$METATOKEN_CONTRACT_ADDRESS'", "meta_vote_contract_address": "'$METAVOTE_CONTRACT_ADDRESS'", "voting_period":'$VOTING_PERIOD', "min_voting_power_amount": "'$MIN_VOTING_POWER_AMOUNT'", "mpip_storage_near_cost_per_kilobytes": "'$MPIP_STOGARE_COST_KB'", "quorum_floor": '$QUORUM_FLOOR' }' --accountId $METAVOTE_CONTRACT_ADDRESS
 
 # Redeploy Contract
-# echo REDEPLOY ONLY
-# near deploy --wasmFile $METAVOTE_WASM --accountId $METAVOTE_CONTRACT_ADDRESS
+echo REDEPLOY ONLY
+near deploy $METAVOTE_CONTRACT_ADDRESS $METAVOTE_WASM 
 
 # reDeploy with MIGRATION - Note: --initFunction caller IS ALWAYS contract-account
-echo reDEPLOY AND STATE MIGRATION
-near deploy $METAVOTE_CONTRACT_ADDRESS $METAVOTE_WASM --initFunction migrate --initArgs {}
+# echo reDEPLOY AND STATE MIGRATION
+# near deploy $METAVOTE_CONTRACT_ADDRESS $METAVOTE_WASM --initFunction migrate --initArgs {}
 
 # NEAR_ENV=testnet near deploy -f --wasmFile $METAVOTE_WASM --accountId metavote.testnet
 # NEAR_ENV=testnet near deploy --wasmFile $METAVOTE_WASM --accountId metavote.testnet
---
+++ scripts/mainnet/view.sh
@@ -0,0 +1,5 @@
+METAVOTE_CONTRACT_ADDRESS="meta-vote.near"
+#NEAR_ENV=testnet near view $METAVOTE_CONTRACT_ADDRESS get_voters '{"from_index":0,"limit":10}'
+NEAR_ENV=mainnet near view $METAVOTE_CONTRACT_ADDRESS get_total_migrated_meta
+NEAR_ENV=mainnet near view $METAVOTE_CONTRACT_ADDRESS get_used_voting_power '{"voter_id":"vhieu.testnet"}'
+
